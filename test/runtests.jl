903aecdd4d1bfe0834c38ac26b4528720c3f7697
dr666m1 <skndr666m1@gmail.com>
2023-09-03 13:18:18 +0900

    add fail test case

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,13 +2,18 @@ using NPS
 using Test
 
 @testset "all" begin
-    @test nps(0) == NPS.detractor
-    @test nps(6) == NPS.detractor
-    @test nps(7) == NPS.passive
-    @test nps(8) == NPS.passive
-    @test nps(9) == NPS.promoter
-    @test nps(10) == NPS.promoter
+    @testset "integer" begin
+        @test nps(0) == NPS.detractor
+        @test nps(6) == NPS.detractor
+        @test nps(7) == NPS.passive
+        @test nps(8) == NPS.passive
+        @test nps(9) == NPS.promoter
+        @test nps(10) == NPS.promoter
+        @test_throws DomainError nps(-1)
+    end
 
-    @test nps([0, 9]) == 0.0
-    @test nps([7, 8, 9]) == 1 / 3 * 100
+    @testset "array" begin
+        @test nps([0, 9]) == 0.0
+        @test nps([7, 8, 9]) == 1 / 3 * 100
+    end
 end
