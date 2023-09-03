c089e8af0749e737524d184b7a819ddf85749e6c
dr666m1 <skndr666m1@gmail.com>
2023-09-03 10:51:29 +0900

    add nps()

--- src/NPS.jl
+++ src/NPS.jl
@@ -1,5 +1,20 @@
 module NPS
 
-greet() = "Hello World!"
+export nps
 
-end # module NPS
+@enum Category promoter passive detractor
+
+function nps(x::Integer)::Category
+    if 0 <= x <= 6
+        return detractor
+    elseif 7 <= x <= 8
+        return passive
+    elseif 9 <= x <= 10
+        return promoter
+    end
+
+    err = DomainError(x, "`x` must be between 0 and 10")
+    return throw(err)
+end
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,10 @@ using NPS
 using Test
 
 @testset "all" begin
-    @test 1 == 1
-    @test NPS.greet() == "Hello World!"
+    @test nps(0) == NPS.detractor
+    @test nps(6) == NPS.detractor
+    @test nps(7) == NPS.passive
+    @test nps(8) == NPS.passive
+    @test nps(9) == NPS.promoter
+    @test nps(10) == NPS.promoter
 end
